c87108e07ff7ed9fabd2d41457d11d8c37c22c03
Damian153 <damiancoman11@gmail.com>
2024-06-05 21:07:53 +0300

    update car

--- blue_car.gd
+++ blue_car.gd
@@ -1,38 +1,47 @@
 extends Area2D
 
 @onready var path_follow_2d = $Path2D/PathFollow2D
-@export var speed = 300
+@export var speed = 230
 @onready var animated_sprite_2d = $Path2D/PathFollow2D/AnimatedSprite2D
 @onready var texture_button = $"../TextureButton"
 var direction = 1
-var old_pos
-var pos
-var moving = false
+var start_pos
+var x
+var y
 var collected = false
+var can_move : int = 0
 func _ready():
-	old_pos = global_position
-	pos = global_position
+	x = path_follow_2d.position.x
+	y = path_follow_2d.position.y
+	start_pos = path_follow_2d.progress
+	can_move = 1
 
 func _process(delta):
-	path_follow_2d.progress += speed * delta * direction
+	x = path_follow_2d.position.x
+	y = path_follow_2d.position.y
+	
+	path_follow_2d.progress += speed * delta * can_move
 	
 	if path_follow_2d.progress_ratio == 1:
-		direction = -1
-	elif path_follow_2d.progress_ratio == 0 and collected:
-		direction = 1
-		collected = false
+		path_follow_2d.progress_ratio = 0
+		can_move = 0
+	if path_follow_2d.progress == start_pos and can_move:
+		can_move = 1
 	
-	pos = global_position
-	if pos - old_pos:
-		moving = true
-		print (pos - old_pos)
+	print (path_follow_2d.progress_ratio)
+	
+	if path_follow_2d.position.x - x:
+		animated_sprite_2d.animation = "left"
+	
+	elif path_follow_2d.position.x > y:
+		animated_sprite_2d.animation = "up"
 	else:
-		moving = false
-	old_pos = pos
+		animated_sprite_2d.animation = "down"
+	
 	
 	
 	
 
 
 func _on_texture_button_pressed():
-	collected = true
+	can_move = 1
